1eb4c082206c2d36e432018fc51e98282bc9e22c
Andrii Horlov <andreygorlovv@gmail.com>
2024-12-18 19:45:43 +0000

    Add more messages and settings to configs

--- Assets/Chess/Data/GameConfig.cs
+++ Assets/Chess/Data/GameConfig.cs
@@ -6,8 +6,10 @@ namespace Chess.Data
     public class GameConfig : ScriptableObject
     {
         [field: SerializeField] public int MouseButtonId { get; private set; }
-
-        [field: SerializeField] private string DefaultAssetPath { get; set; } =
+        [field: SerializeField] public string PivotObjectName { get; private set; }
+        [field: SerializeField] public string SpawnObjectName { get; private set; } = "GLTF Model";
+        
+        [field: Header("Path"), SerializeField] private string DefaultAssetPath { get; set; } =
             "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/d7a3cc8e51d7c573771ae77a57f16b0662a905c6/2.0/ABeautifulGame/glTF/ABeautifulGame.gltf";
         [field: SerializeField] private string AnalogAssetPath { get; set; } =
             "file://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/d7a3cc8e51d7c573771ae77a57f16b0662a905c6/2.0/ABeautifulGame/glTF/ABeautifulGame.gltf";
--- Assets/Chess/Data/MessagesConfig.cs
+++ Assets/Chess/Data/MessagesConfig.cs
@@ -7,6 +7,8 @@ namespace Chess.Data
     {
         [field: Header("Messages"), SerializeField]
         public string CantFindGtlfPath { get; private set; } = "Can't find a glTF model on the path";
+        [field: SerializeField] public string CantFindPivotObject { get; private set; } = "Can't find the object with name: ";
+        [field: SerializeField] public string SceneCantBeLoaded { get; private set; } = "Something happened during loading model from glTF.";
 
         [field: SerializeField] public MessageInformation[] MessageConfigs { get; private set; }
     }
